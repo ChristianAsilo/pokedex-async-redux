15e7c164a58b72dfec54b4a7c70c33406b71aa13
Christian Asilo <asilochristian6@gmail.com>
2023-03-20 18:19:47 +0800

    feat: removed @Default - Removed @Default in pokemon model

--- lib/api/model/pokemon.dart
+++ lib/api/model/pokemon.dart
@@ -6,10 +6,7 @@ part 'pokemon.g.dart';
 
 @freezed
 class Pokemon with _$Pokemon {
-  factory Pokemon({
-    @Default('') @JsonKey(name: 'name') String name,
-    @Default('') @JsonKey(name: 'url') String url,
-  }) = _Pokemon;
+  factory Pokemon() = _Pokemon;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
 }
