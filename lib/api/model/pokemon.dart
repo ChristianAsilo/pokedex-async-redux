2f0ceeb711b3be25474d1293511724ea5e98d009
Christian Asilo <asilochristian6@gmail.com>
2023-03-20 19:06:48 +0800

    feat:get pokemon list - Removed @Default - Resolved feedbacks

--- lib/api/model/pokemon.dart
+++ lib/api/model/pokemon.dart
@@ -6,7 +6,10 @@ part 'pokemon.g.dart';
 
 @freezed
 class Pokemon with _$Pokemon {
-  factory Pokemon() = _Pokemon;
+  factory Pokemon({
+    @JsonKey(name: 'name') required String name,
+    @JsonKey(name: 'url') required String url,
+  }) = _Pokemon;
 
   factory Pokemon.fromJson(Map<String, dynamic> json) => _$PokemonFromJson(json);
 }
