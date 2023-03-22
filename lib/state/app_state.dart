495a2808219dcabcf2428212badfefefa2ff2169
Christian Asilo <asilochristian6@gmail.com>
2023-03-22 12:01:03 +0800

    feat: flutter linting - Modified widget_test to HomePage - Added invalid_annotaion_taget in analysis_option

--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -5,13 +5,11 @@ part 'app_state.freezed.dart';
 part 'app_state.g.dart';
 
 @freezed
-class AppState with  _$AppState {
-
+class AppState with _$AppState {
   factory AppState({
     //TODO: this will be removed
-    @Default('')String sample,
-}) = _AppSate;
+    @Default('') String sample,
+  }) = _AppSate;
 
   factory AppState.fromJson(Map<String, dynamic> json) => _$AppStateFromJson(json);
 }
-
