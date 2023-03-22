d843dc273cad6b93c456cfa5ca7d8beae08df2b7
Christian Asilo <asilochristian6@gmail.com>
2023-03-22 16:45:31 +0800

    feat: implemented pokemons action - Added new list of pokemon in AppState - Added constants.dart - Removed Future in main.dart - Created GetPokemonsAction class

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,14 @@
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex_async_redux/state/action/pokemon_actions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    StoreProvider.dispatch(context, GetPokemonsAction());
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:pokedex_async_redux/api/model/api_service.dart';
 import 'package:pokedex_async_redux/home_page.dart';
 import 'package:pokedex_async_redux/state/app_state.dart';
 
-//TODO: Future will be removed later
-Future<void> main() async {
+void main() {
   final store = Store<AppState>(
     initialState: AppState(),
     actionObservers: kReleaseMode ? null : [ConsoleActionObserver<AppState>()],
   );
-
-  //TODO: To be removed later
-  final pokemons = await ApiService().pokemonApi.getPokemonList(offset: '0', limit: '151');
-  pokemons;
-
-  //TODO: To be removed later
-  final pokemonDetails = await ApiService().pokemonApi.getPokemonDetails(name: 'charizard');
-  pokemonDetails;
 
   runApp(
     StoreProvider(
