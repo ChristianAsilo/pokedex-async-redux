a9500cb8017e4674255e9e756905ffba373c127b
Christian Asilo <asilochristian6@gmail.com>
2023-03-24 18:20:29 +0800

    feat: Added overview page to display pokemons (pr feedback) - Removed unnecessary constants - Renamed indexString into indexKey - Passed the whole Pokemon model in pokemonCard

--- lib/features/pokemon_overview/pokemon_overview_page.dart
+++ lib/features/pokemon_overview/pokemon_overview_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_async_redux/api/model/pokemon.dart';
-import 'package:pokedex_async_redux/utils/constants.dart';
 import 'package:pokedex_async_redux/widget/pokemon_card.dart';
 
 class PokemonOverviewPage extends StatelessWidget {
@@ -13,20 +12,16 @@ class PokemonOverviewPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    pokemons;
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text(appBarTitle)),
+        appBar: AppBar(title: const Text('Pokedex')),
         body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: gridCount),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           itemCount: pokemons.length,
           itemBuilder: (_, index) {
             final pokemon = pokemons[index];
-            final pokemonId = pokemon.url.split('/')[6];
-            final pokemonFinalUrl = imageLink.replaceFirst(indexString, pokemonId);
             return PokemonCard(
-              pokemonName: pokemon.name,
-              pokemonImage: pokemonFinalUrl,
+              pokemon: pokemon,
             );
           },
         ),
--- lib/widget/pokemon_card.dart
+++ lib/widget/pokemon_card.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex_async_redux/api/model/pokemon.dart';
 import 'package:pokedex_async_redux/utils/constants.dart';
 
 class PokemonCard extends StatelessWidget {
   const PokemonCard({
-    required this.pokemonName,
-    required this.pokemonImage,
+    required this.pokemon,
     Key? key,
   }) : super(key: key);
 
-  final String pokemonName;
-  final String pokemonImage;
+  final Pokemon pokemon;
 
   @override
   Widget build(BuildContext context) {
+    final pokemonId = pokemon.url.split('/')[6];
+    final pokemonImageUrl = imageLink.replaceFirst(indexKey, pokemonId);
     return Card(
       child: Padding(
-        padding: cardPadding,
+        padding: const EdgeInsets.all(20),
         child: Column(
-          crossAxisAlignment: crossAlignmentCenter,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              pokemonName,
-              style: textSize,
+              pokemon.name,
+              style: const TextStyle(fontSize: 20),
             ),
             Expanded(
-              child: Image.network(pokemonImage),
+              child: Image.network(pokemonImageUrl),
             ),
           ],
         ),
